3b939130dfdc5b88a90b37abdebd40f9fdebd46c
jun kakizako <bai2oo5s@gmail.com>
2026-06-16 06:45:59 +0900

    クエリパラメータで県の絞り込み追加

--- backend/aging/views.py
+++ backend/aging/views.py
@@ -3,5 +3,17 @@ from django.http import JsonResponse
 
 # Create your views here.
 def aging_list(request):
-    data = [{"city": "大分市", "aging_rate": 0.31},{"city": "別府市", "aging_rate": 0.36}, {"city": "中津市", "aging_rate": 0.33}]
-    return JsonResponse(data, json_dumps_params={"ensure_ascii": False}, safe= False)
+    data = [
+        {"pref": "大分県","city": "大分市", "aging_rate": 0.31},
+        {"pref": "大分県","city": "別府市", "aging_rate": 0.36}, 
+        {"pref": "福岡県","city": "福岡市", "aging_rate": 0.33}
+        ]
+    pref = request.GET.get("pref")
+    result = []
+    if pref:
+        for item in data:
+            if item["pref"] == pref:
+                result.append(item)
+    else:
+        result = data
+    return JsonResponse(result, json_dumps_params={"ensure_ascii": False}, safe= False)
