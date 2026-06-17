13fb2548ae24f4cd90ab23a5ea77e82710108a8e
jun kakizako <bai2oo5s@gmail.com>
2026-06-17 23:11:30 +0900

    e-Stat生データの整形処理を追加（コード変換・level分類）

--- backend/experiment.py
+++ backend/experiment.py
@@ -15,8 +15,53 @@ response = requests.get(url, params= params)
 data = response.json()
 
 values = data["GET_STATS_DATA"]["STATISTICAL_DATA"]["DATA_INF"]["VALUE"]
-print(len(values))
-print(values[0])
+# print(len(values))
+# print(values[0])
 
 times = set(v["@time"] for v in values)
-print(times)
+# print(times)
+# print(data["GET_STATS_DATA"]["STATISTICAL_DATA"]["CLASS_INF"]["CLASS_OBJ"])
+
+area_map = {}
+for i in data["GET_STATS_DATA"]["STATISTICAL_DATA"]["CLASS_INF"]["CLASS_OBJ"]:
+    if i["@id"] == "area":
+        area_class = i["CLASS"]
+        for v in i["CLASS"]:
+            area_map[v["@code"]] = {"name": v["@name"],"level": v["@level"]}
+            # area_map["name"] = v["@name"]
+            
+# print(len(area_map))
+# print(area_map)
+
+result = []
+for v in values:
+    code = v["@area"]
+    info = area_map[code]
+    level = info["level"]
+    if level in ("3", "5"):
+        continue
+    if level == "2":
+        type = "都道府県"
+    else:
+        type = "市区町村"
+
+    item = {
+        "city": info["name"],
+        "aging_rate": float(v["$"]),
+        "type": type,
+    }
+    result.append(item)
+
+print(len(result))
+print([r for r in result if r["type"] == "都道府県"][:3])
+
+leves = set(v["@level"] for v in area_class)
+# print(leves)
+# print(result[:5])
+
+level_example = {}
+for v in area_class:
+    if v["@level"] not in level_example:
+        level_example[v["@level"]] = v["@name"]
+
+# print(level_example)
